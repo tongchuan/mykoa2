e7fc1d1b1a4b70ef983afd69c696807d4d5366c5
zhangtch <zhangtch@yonyou.com>
2021-08-12 20:30:50 +0800

    update

--- dist/ws.js
+++ dist/ws.js
@@ -1,9 +1,4 @@
 "use strict";
-var __spreadArray = (this && this.__spreadArray) || function (to, from) {
-    for (var i = 0, il = from.length, j = to.length; i < il; i++, j++)
-        to[j] = from[i];
-    return to;
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -13,13 +8,20 @@ var koa_router_1 = __importDefault(require("koa-router"));
 var koa_websocket_1 = __importDefault(require("koa-websocket"));
 var app = koa_websocket_1.default(new koa_1.default());
 var router = new koa_router_1.default();
+// const clients = new Set<websocket.MiddlewareContext<Koa.DefaultState> & Koa.DefaultContext>();
+var clients = [];
 app.ws.use(function (ctx, next) {
+    // clients.push(ctx);
+    // console.log(ctx)
     return next(ctx);
 });
-var clients = new Set();
+// app.ws.use((ctx, next) => {
+//   clients.add(ctx);
+//   return next();
+// });
 router.all('/test/:id', function (ctx) {
-    clients.add(ctx.websocket);
-    // console.log(ctx)
+    // console.log(ctx.websocket)
+    clients.push(ctx.websocket);
     // `ctx` is the regular koa context created from the `ws` onConnection `socket.upgradeReq` object.
     // the websocket is added to the context on `ctx.websocket`.
     ctx.websocket.send('Hello World');
@@ -29,13 +31,14 @@ router.all('/test/:id', function (ctx) {
     });
 });
 setTimeout(function () {
-    console.log("OK", __spreadArray([], clients).length, JSON.stringify(clients));
-    __spreadArray([], clients).forEach(function (client) {
-        console.log("OK", JSON.stringify(client));
-        //     console.log('websocket',client.websocket)
-        client.send(JSON.stringify(client));
+    // console.log('OK', clients.length, JSON.stringify(clients));
+    clients.forEach(function (client) {
+        client.send('OK');
+        // console.log('OK', JSON.stringify(client));
+        //   //     console.log('websocket',client.websocket)
+        //   client.send(JSON.stringify(client));
     });
 }, 5000);
-app.ws.use(router.routes());
+app.ws.use(router.routes()).use(router.allowedMethods());
 app.listen(3001);
 //# sourceMappingURL=ws.js.map
--- src/ws.ts
+++ src/ws.ts
@@ -4,31 +4,41 @@ import websocket from 'koa-websocket';
 
 const app = websocket(new Koa());
 const router = new Router();
+// const clients = new Set<websocket.MiddlewareContext<Koa.DefaultState> & Koa.DefaultContext>();
+const clients = []
 app.ws.use((ctx, next) => {
-    return next(ctx);
-});
-let clients = new Set()
-router.all('/test/:id', (ctx) => {
-    clients.add(ctx.websocket)
-    // console.log(ctx)
-    // `ctx` is the regular koa context created from the `ws` onConnection `socket.upgradeReq` object.
-    // the websocket is added to the context on `ctx.websocket`.
-    ctx.websocket.send('Hello World');
-    ctx.websocket.on('message', (message: any) => {
-        // do something with the message from client
-            console.log(message);
-    });
+  // clients.push(ctx);
+  // console.log(ctx)
+  return next(ctx);
 });
 
+// app.ws.use((ctx, next) => {
+//   clients.add(ctx);
+//   return next();
+// });
 
-    setTimeout(()=>{
-        console.log("OK",[...clients].length,JSON.stringify(clients));
-        [...clients].forEach((client)=>{
-            console.log("OK",JSON.stringify(client));
-        //     console.log('websocket',client.websocket)
-            client.send(JSON.stringify(client))
-        })
-    }, 5000);
+router.all('/test/:id', (ctx: any) => {
 
-app.ws.use(router.routes());
+  // console.log(ctx.websocket)
+  clients.push(ctx.websocket)
+  // `ctx` is the regular koa context created from the `ws` onConnection `socket.upgradeReq` object.
+  // the websocket is added to the context on `ctx.websocket`.
+  ctx.websocket.send('Hello World');
+  ctx.websocket.on('message', (message: any) => {
+    // do something with the message from client
+    console.log(message);
+  });
+});
+
+
+setTimeout(() => {
+  // console.log('OK', clients.length, JSON.stringify(clients));
+  clients.forEach((client) => {
+    client.send('OK');
+    // console.log('OK', JSON.stringify(client));
+  //   //     console.log('websocket',client.websocket)
+  //   client.send(JSON.stringify(client));
+  });
+}, 5000);
+app.ws.use((router as any).routes()).use((router as any).allowedMethods());
 app.listen(3001);
